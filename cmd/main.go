99e15dfa3913de39d2c4796b323dcd985993bd81
Shachar Sharon <ssharon@redhat.com>
2022-08-22 12:17:44 +0300

    main: test deployment over minikube
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	"github.com/go-logr/logr"
 	"github.com/synarete/nfs-ganesha-metrics/internal/metrics"
 )
 
@@ -24,6 +25,18 @@ func init() {
 
 func main() {
 	log := zap.New(zap.UseDevMode(true))
+
+	// Report global info early upon boot
+	start(log)
+
+	// Do minimal check for DBus readers
+	poke(log)
+
+	// Execute metrics server
+	exec(log)
+}
+
+func start(log logr.Logger) {
 	log.Info("Initializing nfsganeshametrics",
 		"ProgramName", os.Args[0],
 		"GoVersion", goruntime.Version(),
@@ -40,9 +53,28 @@ func main() {
 		"DBUS_SESSION_BUS_ADDRESS",
 		os.Getenv("DBUS_SESSION_BUS_ADDRESS"),
 	)
+}
 
+func poke(log logr.Logger) {
+	exportsReader := metrics.NewExportsDbusReader()
+	if err := exportsReader.Setup(); err != nil {
+		log.Error(err, "ExportsDbusReader.Setup")
+		return
+	}
+	defer exportsReader.Close()
+
+	clientsReader := metrics.NewClientsDbusReader()
+	if err := exportsReader.Setup(); err != nil {
+		log.Error(err, "ClientsDbusReader.Setup")
+		return
+	}
+	defer clientsReader.Close()
+}
+
+func exec(log logr.Logger) {
 	err := metrics.RunNfsgMetricsExporter(log)
 	if err != nil {
+		log.Error(err, "RunNfsgMetricsExporter")
 		os.Exit(1)
 	}
 }
